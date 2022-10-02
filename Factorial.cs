6770ef1ddf1c356f3b0916178def580bd9622895
John Rodolfo <114643834+johnata20@users.noreply.github.com>
2022-10-02 03:29:05 -0600

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,13 @@
+﻿int inp, fact = 1;
+
+Console.Write("Enter an integer number : ");
+inp = Convert.ToInt32(Console.ReadLine());
+
+for (int x = 1; x <= inp; x++)
+    fact = fact * x;
+
+Console.WriteLine("The factorial of {0} is : {1}", inp, fact);
+Console.WriteLine();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
